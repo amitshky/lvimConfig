a52df53cf2475d9534cdc94a23d4021359421c8d
amitshky <a7shky@gmail.com>
2023-12-25 22:13:25 +0545

    trouble plugin added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,4 +3,8 @@ lvim.plugins = {
   { "nanotech/jellybeans.vim" },
   { "ellisonleao/gruvbox.nvim" },
   { "amitshky/lunarplus.nvim" },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 }
