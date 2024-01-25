ab06565a2d1b64c5e8d52fc6e19c7feebbf4ff9c
amitshky <amitshkymail@gmail.com>
2024-01-25 16:29:00 +0545

    fixing checkOS

--- lua/user/lvim/settings.lua
+++ lua/user/lvim/settings.lua
@@ -1,5 +1,5 @@
-local checkOS = vim.fn.has('macunix')
-if (checkOS == 0) then -- windows
+local checkOS = vim.loop.os_uname().sysname
+if (checkOS == 'Windows') then
   -- Enable powershell as your default shell
   vim.opt.shell = "pwsh.exe -NoLogo"
   vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
